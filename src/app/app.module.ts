05eb0c1687a6e829c20746e2acd25f4b6c45b5af
Shuv0Khan <naweemshuvo@gmail.com>
2022-05-26 12:53:37 -0400

    2.7 to 3.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponenet } from './media-item-list.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemListComponenet,
+    MediaItemComponent
   ],
   bootstrap: [
     AppComponent
